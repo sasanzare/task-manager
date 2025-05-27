0c4a31859f46627e6b103c5728c2a6d4a52e4ec6
sasanzare <sasanzare75@gmail.com>
2025-05-27 14:20:07 +0330

    Add initial dependencies to Cargo.toml

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use actix_cors::Cors;
+use actix_web::{get, http::header, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().json(serde_json::json!({
+        "message": "Hello "
+    }))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        let cors = Cors::default()
+            .allow_any_origin()
+            .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
+            .allowed_headers(vec![header::AUTHORIZATION, header::ACCEPT])
+            .allowed_header(header::CONTENT_TYPE)
+            .max_age(3600);
+
+        App::new()
+            .wrap(cors)
+            .service(hello)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
